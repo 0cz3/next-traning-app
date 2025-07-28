37a8ed6328797e14c7faff9aa64051593b35d6cf
濱口 菫 <sumire_h2@waku-2.com>
2025-07-28 18:35:14 +0900

    POSTメソッド追加

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,14 +1,23 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export const GET = async() => {
+export const GET = async () => {
   try {
     // modelで定義したTaskの全記事取得
     const tasks = await prisma.task.findMany();
-    return NextResponse.json({message: "success", tasks}, {status: 200})
+    return NextResponse.json({ message: "success", tasks }, { status: 200 });
   } catch (err) {
-    // エラー発生時のレスポンス
-    return NextResponse.json({message: "Error", err}, {status: 500})
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
   }
 };
-console.log("GET");
+
+export const POST = async (req: Request) => {
+  try {
+    const { taskName, deadline } = await req.json();
+    // TODO バリデーション追加
+    const tasks = await prisma.task.create({ data: { taskName: taskName, deadline: new Date(deadline) } });
+    return NextResponse.json({ message: "success", tasks }, { status: 201 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  }
+};
