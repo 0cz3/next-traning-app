260c6d8d178f10f4a5715c48fd9f7bbc462baa97
濱口 菫 <sumire_h2@waku-2.com>
2025-09-29 16:48:45 +0900

    page.tsxへ例外処理文追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,16 @@ import "./globals.css";
 export default async function Home() {
   const API_URL = process.env.NEXT_PUBLIC_API_URL!;
   // cache: "no-store" = SSR 動的データ取得 更新頻度高
-  const res = await fetch(`${API_URL}/api/todo`, { cache: "no-store" });
-  const tasks = await res.json();
+  const res = await fetch(`${API_URL}/api/todo`, { cache: "no-store" }).catch((err)=>{
+    console.log(err)
+    console.log(API_URL)
+  })
+  let tasks
+  if(!res){
+    tasks = ""
+  } else {
+    tasks = await res.json();
+  }
   console.log(`tasks${tasks}`)
   return (
     <div className="todo">
